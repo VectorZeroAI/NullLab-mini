1432d774faf8ff8f978b3b2caf4306befd752272
Null <VectorZeroAI@gmail.com>
2025-12-26 16:23:39 +0100

    Trying to get ai_chat.py done. Encoutered an LSP error. It says the ariable is undefined, but the variable is 100% clearly defined correctly. IDK WTF is happening there.

--- ai_chat.py
+++ ai_chat.py
@@ -27,7 +27,6 @@ print("[green]Colors initiated. =)[/green]")
 import requests
 import sys
 import LMIA_context_mini
-from mygo import enable_goto, goto, label
 
 print("[green]Everything else initialised[/green]")
 
@@ -45,12 +44,19 @@ headers = {
 stage = 1
 mem = LMIA_context_mini.LMIA_context_mini(DB_path="./DB.db") # Memory initialisation
 # TODO: Add a loading of stage from a save file, if such exists. 
-label("main_programm_loop")
+
+
+global error_action
+error_action = None
+
+# Main loop
 while stage in (1, 2, 3, 4):
+    global prev_stage
     # This is the user input handling
     user_input = input("You: ").strip()
     if user_input.lower() in ("exit", "quit"):
         prev_stage = stage
+        error_action = "exit"
         break
     mem.input_context(user_input, 1) # NOTE: 0 == ai , 1 == user
 
@@ -81,24 +87,18 @@ while stage in (1, 2, 3, 4):
         pass
     elif stage == 4:
         pass
-else:
-    print("[green]It was nice to build this with you.[/green]")
-    sys.exit("FINISHED")
 
-"""
-If the program runs well, it would exit already, with the else: after while. Stage will go to a different value, and it will finish. 
-But this is a shit design.... ... whatever! 
-So everything below the intented block above is only executed if the loop was broken, wich is my way of handling exeptions and edge cases. 
-They all get here, into this shitcode place, where they will be handled, and possibly just gone upwards via goto. Or I will wrap everything into anouther loop. Didnt decide yet. 
-I guess goto(loop) is cleaner in this situation, then any structured solution. 
-"""
-
-print("[red]shutting down.[/red]")
-print("[red]FEATURE OF SAVING PROGRESS NOT IMPLEMENTED[/red]")
-print("[red]You sure you want to exit[/red]")
-print("[red][bold]???[/bold][/red]")
-if input("Answer: ").strip().lower() in ("no", "n", "nope", "never", "nah"):
-    stage = prev_stage
-    goto("main_programm_loop")
+    # ERROR ACTION handling here. This is kinda the shit that should handle everything. 
+    if error_action is not None:
+        if error_action is "exit":
+            print("[red]shutting down.[/red]")
+            print("[red]FEATURE OF SAVING PROGRESS NOT IMPLEMENTED[/red]")
+            print("[red]You sure you want to exit[/red]")
+            print("[red][bold]???[/bold][/red]")
+            if input("Answer: ").strip().lower() in ("no", "n", "nope", "never", "nah"):
+                stage = prev_stage # NO ITS NOT UNDEFINED!!!
+                error_action = None
+            else:
+                raise NotImplementedError("Yep, its still pre alfa, what ya expecting?")
 else:
-    raise NotImplementedError("Yep, its still pre alfa, what ya expecting?")
+    sys.exit("WTF HAPPENED HERE?")
---
+++ config.py
@@ -0,0 +1,10 @@
+"""
+Config file for NullLab-mini. 
+"""
+
+# ai_chat.py values:
+
+api_key = "set your API key here"
+model = "set your model here"
+
+# NOTE: THIS IS NOT FINISHED!!!
